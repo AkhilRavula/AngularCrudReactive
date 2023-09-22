8610ec8f3895b1c4a9b0b88efe6d3d999667611f
Akhil <ravulaakhil123@gmail.com>
2023-09-23 00:05:51 +0530

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -22,6 +22,7 @@ import { NotFoundErrorComponent } from './ErrrorComponent/NotFoundError/NotFound
 import { InternalServerErrorComponent } from './ErrrorComponent/InternalServerError/InternalServerError.component';
 import { GlobalErrorHandlerService } from './Services/global-error-handler.service';
 import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule, EmployeemoduleModule,SharedModule,
     AppRoutingModule,HttpClientModule,
-    ReactiveFormsModule,BrowserAnimationsModule ,ToastrModule.forRoot(), CarouselModule.forRoot()
+    ReactiveFormsModule,BrowserAnimationsModule ,ToastrModule.forRoot(), CarouselModule.forRoot(), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})
 
   ],
   providers: [employeeservice,AuthServiceService,GlobalErrorHandlerService],
